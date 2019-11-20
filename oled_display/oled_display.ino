f456b9c4c7474db509f06500fbb1f26c1cdf88c7
JiJiao Feng <16521693+fengjijiao@users.noreply.github.com>
2019-11-20 16:30:00 +0800

    Update oled_display.ino

--- oled_display/oled_display.ino
+++ oled_display/oled_display.ino
@@ -1,6 +1,6 @@
 #include <U8glib.h>
 /*SPI协议*/
-U8GLIB_SSD1306_128X64 u8g(13, 11, 10, 9); // SW SPI Com: SCK = 13, MOSI = 11, CS = 10, A0 = 9 
+U8GLIB_SSD1306_128X64 u8g(13, 11, 10, 9); // SW SPI Com: SCK = 13, MOSI = 11, CS = 10, A0 = 9 
 void setup()
 {
   if (u8g.getMode()==U8G_MODE_R3G3B2){
@@ -12,16 +12,27 @@ void setup()
   }
   // u8g.setFont(u8g_font_unifont);
   Serial.begin(9600);
+  //u8g.setFont(u8g_font_gdr25);
   u8g.setFont(u8g_font_6x10);
   u8g.setFontRefHeightExtendedText();
   u8g.setDefaultForegroundColor();
   u8g.setFontPosTop();
 }
+//变化显示A0模拟数值量
+void draw()
+{
+  char buffer[20];
+  char front[]="A0:";
+  float a=analogRead(0);
+  itoa(a,buffer,10);
+  strcat(front,buffer);
+  u8g.drawStr(0,0,front);
+}
 void loop()
 {
   u8g.firstPage();
   do{
-    u8g.drawStr(0,0,"latency:33ms");
+    draw();
   }while(u8g.nextPage());
   delay(500);
 }
